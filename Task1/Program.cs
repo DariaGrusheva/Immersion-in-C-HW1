8ea371ff454b66a263a2a9808dca9120c1b59a96
Daria <gdaria1609@yandex.ru>
2023-11-16 15:21:15 +0500

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿
+// Написать программу-калькулятор, вычисляющую выражения вида a + b, a - b, a / b, a * b, введенные из командной строки,
+// и выводящую результат выполнения на экран.
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+                if (int.TryParse(args[0], out int num1) && int.TryParse(args[2], out int num2))
+                {
+                    switch (args[1])
+                    {
+                        case "+":
+                            Console.WriteLine($"{num1} + {num2} = {num1 + num2}.");
+                            break;
+                        case "-":
+                            Console.WriteLine($"{num1} - {num2} = {num1 - num2}.");
+                            break;
+                        case "*":
+                            Console.WriteLine($"{num1} * {num2} = {num1 * num2}.");
+                            break;
+                        case "/":
+                            Console.WriteLine($"{num1} / {num2} = {num1 / num2} остаток {num1 - (num1 / num2 * num2)}.");
+                            break;
+                        default:
+                            Console.WriteLine("Введенная Вами операция некорректна!");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели не числа!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели пример некорректно!");
+            }
+        }
+    }
+}
